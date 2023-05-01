b75dcc67f4264bae9a145e483a29343da6390b6a
Orest Klymko <116631168+OrestKlymko@users.noreply.github.com>
2023-05-01 09:19:49 +0200

    delete favi

--- src/components/v-footer.vue
+++ src/components/v-footer.vue
@@ -20,7 +20,7 @@
         ></a>
       </li>
     </ul>
-    <p>©2023 Orest Klymko | All Rights Reserved</p>
+    <p>©2023 by Orest Klymko | All Rights Reserved</p>
   </footer>
 </template>
 
@@ -33,36 +33,35 @@ export default {
 <style scoped>
 @import url("https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800,900&display=swap");
 * {
-  margin: 0; /*обнуление отступов*/
-  padding: 0; /*обнуление полей*/
-  box-sizing: border-box; /*метод расчета размеров бокса*/
-  font-family: "Poppins", sans-serif; /*название шрифта*/
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Poppins", sans-serif;
 }
 body {
-  display: flex; /*флексбокс сетка*/
-  flex-direction: column; /*направление главной оси*/
-  justify-content: flex-end; /*элементы в конце контейнера*/
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
   min-height: 100vh;
 }
 footer {
-  position: relative; /*относительное позиционирование*/
-  width: 100%; /*на всю ширину*/
-  padding: 20px 50px; /*поля для контента*/
-  background: #81aded; /*цвет футера*/
+  position: relative;
+  width: 100%;
+  padding: 20px 50px;
+  background: #81aded;
   display: flex;
-  justify-content: center; /*в центре по горизонтали*/
-  align-items: center; /*в центре по вертикали*/
+  justify-content: center;
+  align-items: center;
   flex-direction: column;
-  margin-top: 100px; /*отступ сверху*/
+  margin-top: 100px;
 }
 footer .wave {
-  position: absolute; /*абсолютное позиционирование*/
+  position: absolute;
   top: -100px;
   left: 0;
   width: 100%;
   height: 100px;
-  /* background: url(wave.png); картинка волны */
-  background-size: 1000px 100px; /*ширина и высота волны*/
+  background-size: 1000px 100px;
 }
 .social {
   position: relative;
@@ -70,25 +69,25 @@ footer .wave {
   justify-content: center;
   align-items: center;
   margin: 10px 0;
-  flex-wrap: wrap; /*разрешен перенос на новую строку*/
+  flex-wrap: wrap;
 }
 .social li,
 .menu li {
-  list-style: none; /*удалить черные маркеры*/
+  list-style: none;
 }
 .social li a {
-  font-size: 2em; /*размер иконок*/
-  color: #fff; /*цвет иконок*/
+  font-size: 2em;
+  color: #fff;
   margin: 0 10px;
-  display: inline-block; /*отображать как строчно-блочный элемент*/
-  transition: 0.5s; /*плавный переход*/
+  display: inline-block;
+  transition: 0.5s;
 }
 .menu li a {
-  font-size: 1.2em; /*размер ссылок меню*/
-  color: #fff; /*цвет ссылок меню*/
-  opacity: 0.75; /*значение прозрачности*/
+  font-size: 1.2em;
+  color: #fff;
+  opacity: 0.75;
   margin: 0 15px;
-  text-decoration: none; /*ссылка без подчеркивания*/
+  text-decoration: none;
   display: inline-block;
 }
 .menu li a:hover {
